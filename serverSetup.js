335126e71d81d5eab69176896ad8383aa3730d59
HexVanilla <97599030+HexVanilla@users.noreply.github.com>
2023-10-08 18:56:16 -0300

    cors testing

--- serverSetup.js
+++ serverSetup.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const { createServer } = require('http')
 const { Server } = require('socket.io')
+const cors = require('cors')
 
 const app = express()
+app.use(
+  cors({
+    origin: /^https:\/\/tap-tap-pizza-client(-[a-z0-9]+)?\.vercel\.app$/,
+    credentials: true,
+    methods: ['GET', 'POST'],
+  })
+)
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
   cors: {
